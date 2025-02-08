6d1927d1a2b45b85aff2ebca1bae6ccb3d01721b
Sanchit Aggarwal <aggarwalsanchit001@gmail.com>
2025-02-08 19:35:48 +0530

    starting with building home page of  krishcare

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import {cn} from '@/lib/utils'
+import { ThemeProvider } from '@/components/ui/theme-provider'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const fontsans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  weight :['300', '400', '500', '600', '700'],
+  variable: '--font-sans'
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "krishcare",
@@ -24,11 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontsans.variable)}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+          >
+            {children}
+            </ThemeProvider>
+            </body>
+        
+      
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,43 @@
+import PatientForm from '@/components/ui/Forms/PatientForm';
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function Home() {
   return (
-    <div>
-      <h1> Hello</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+        <div className="flex items-center gap-2 mb-12">
+          <Image
+            src="/assets/icons/logo-icon.svg"
+            height={1000}
+            width={1000}
+            alt='patient-img'
+            className='h-10 w-fit'
+          />
+          <span className="text-2xl font-bold text-white header">KrishCare</span>
+        </div>
+        <PatientForm />
+
+        <div className='text-14-regular mt-20 flex justify-between'>
+          <p className='justify-items-end text-dark-600 xl:text-left'>
+            ©2025 krishcare
+          </p>
+          <Link href='/?admin=true' className='text-green-500'>
+            Admin
+          </Link>
+        </div>
+        </div>
+      </section>
+       <Image
+       src='/assets/images/onboarding-img.png'
+       height={1000}
+       width={1000}
+       alt='patient'
+       className='side-img max-w-[50%]'
+
+       />
     </div>
   );
 }
